0f5ef7c242cc60426ed9c11a20e5737eedba357c
phgrund <phrgrund@gmail.com>
2026-03-20 14:24:17 -0300

    Refactor load_k9s_aliases function to use eval for alias commands and improve CSV parsing

--- k9s_aliases.sh
+++ k9s_aliases.sh
@@ -5,17 +5,22 @@ K9S_CONTEXTS_FILE="$HOME/.ph-profile/k9s_contexts.csv"
 load_k9s_aliases() {
     [ -f "$K9S_CONTEXTS_FILE" ] || return 0
 
-    while IFS=',' read -r alias_name aws_profile cluster_context; do
-        [ -n "$alias_name" ] || continue
-
-        case "$alias_name" in
-            \#*)
-                continue
-                ;;
-        esac
-
-        alias "$alias_name=k9s_sso $aws_profile $cluster_context"
-    done < "$K9S_CONTEXTS_FILE"
+    while IFS= read -r alias_cmd; do
+        [ -n "$alias_cmd" ] || continue
+        eval "$alias_cmd"
+    done <<EOF
+$(awk -F',' '
+BEGIN { OFS="," }
+NR==1 && $1 ~ /^#/ { next }
+$1 ~ /^#/ { next }
+{
+    gsub("\r", "", $1); gsub("\r", "", $2); gsub("\r", "", $3);
+    if (length($1) > 0) {
+        gsub(/"/, "\\\"", $2); gsub(/"/, "\\\"", $3);
+        print "alias " $1 "=\"k9s_sso " $2 " " $3 "\""
+    }
+}' "$K9S_CONTEXTS_FILE")
+EOF
 }
 
 load_k9s_aliases
